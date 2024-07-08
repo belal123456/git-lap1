cfce26d646f900f1c7c32bb3b6930e7459a470cc
belal_soliman <belalsoliman8@gmail.com>
2024-07-08 17:57:40 +0300

    change hello world class name

---
+++ hello-java.js
@@ -0,0 +1,10 @@
+class Greeter {
+  // Method to print "Hello, Java!"
+  printHelloJava() {
+    console.log("Hello, Java!");
+  }
+}
+
+// Example usage:
+const greeter = new Greeter();
+greeter.printHelloJava(); // This will print "Hello, Java!" to the console
