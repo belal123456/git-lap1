a12c8a34fc3165301acc0e8c8fc227ab37b176cb
belal_soliman <belalsoliman8@gmail.com>
2024-07-08 17:52:05 +0300

    add hello iti class

---
+++ hello-iti.js
@@ -0,0 +1,10 @@
+class Greeter {
+  // Method to print "Hello, ITI!"
+  printHelloITI() {
+    console.log("Hello, ITI!");
+  }
+}
+
+// Example usage:
+const greeter = new Greeter();
+greeter.printHelloITI(); // This will print "Hello, ITI!" to the console
